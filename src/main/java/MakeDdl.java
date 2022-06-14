cf083036e45e0bdba37303faad460f945bd05f98
Marco(양채훈) <76933569+yd9506@users.noreply.github.com>
2022-06-14 17:26:34 +0900

    input 디렉터리 내부에 있는 csv 파일을 읽어 JPA entity로 변환 (#2)
    * 샘플 csv 파일 추가
    * csv 파일을 매개변수로 받지 않고 input 디렉터리 내의 csv 파일을 JPA
    entity로 변환
    * README에 사용법 설명

--- src/main/java/MakeDdl.java
+++ src/main/java/MakeDdl.java
@@ -1,14 +1,22 @@
+import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.util.List;
 
 public class MakeDdl {
     public static void main(String[] args) throws IOException {
-        if (args == null || args.length == 0) {
-            return;
-        }
+        File dir = new File("input");
+        dir.mkdir();
+        FileFilter filter = new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.getName().endsWith(".csv");
+            }
+        };
+        File[] list = dir.listFiles(filter);
 
-        for (int i = 0; i < args.length; i++) {
-            List<List<String>> tableData = CsvReader.read(args[i]);
+        for (int i = 0; i < list.length; i++) {
+            List<List<String>> tableData = CsvReader.read(list[i].getPath());
             Table table = new Table(tableData);
             table.makeJpaEntityFile();
         }
